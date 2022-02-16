eda55efd9b018282a1fc17ee66da6a3e32a2a836
rahul007-bit <yadavrahul8141@yahoo.com>
2022-02-16 10:19:57 +0530

    remove links

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./header/Header";
 import LandingPage from "./landingPage/LandingPage";
